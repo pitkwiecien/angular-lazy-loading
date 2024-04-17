73d924ba520cbab67a585c47e872bc17ce49ab45
pitkwiecien <pitkwiecien@gmail.com>
2024-04-17 10:02:54 +0200

    Made MainModule standalone

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
+  standalone: true,
   styleUrl: './main.component.css'
 })
 export class MainComponent {
